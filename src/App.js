4075ebad0775324004af4f146337a21556f1ff69
Nitish Kumar Jha <pingnitish@gmail.com>
2021-08-28 02:15:50 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Show My Trailer</h1>
+      <h2>React Web App to see trailer</h2>
+    </div>
+  );
+}
